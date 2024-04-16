3f82b51b4dde83ede8a0ae6a7ecae02e4ad68cfa
Ahmed Elnour <ahmedelnour@Ahmeds-Air.fios-router.home>
2024-04-16 18:00:37 -0400

    create home page, add mui

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Link from "next/link";
 
 export const metadata = {
   title: "medElnour",
@@ -8,7 +9,45 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="flex flex-col min-h-screen w-full font-poppins bg-gradient-to-br from-gray-700 via-gray-900 to-black text-white items-center">
+          <Header />
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
+
+const Header = () => {
+  return (
+    <div className="w-full mx-auto sticky top-0">
+      <div className="container mx-auto flex justify-between  p-5 items-end flex-wrap">
+        <Link href={"/"} className="hover:cursor-pointer hover:text-red-600">
+          <span className="text-4xl font-semibold">medElnour</span>
+        </Link>
+
+        <Nav />
+      </div>
+    </div>
+  );
+};
+
+const Nav = () => {
+  return (
+    <div className="hidden md:flex">
+      <Link className="mx-4 hover:border-b-2 border-white" href={"/experience"}>
+        Experience
+      </Link>
+      <Link className="mx-4 hover:border-b-2 border-white" href={"/projects"}>
+        Projects
+      </Link>
+      <Link className="mx-4 hover:border-b-2 border-white" href={"/skills"}>
+        Skills
+      </Link>
+      <Link className="mx-4 hover:border-b-2 border-white" href={"/contact"}>
+        Contact
+      </Link>
+    </div>
+  );
+};
--- app/page.js
+++ app/page.js
@@ -1,9 +1,51 @@
-import Image from "next/image";
+import Link from "next/link";
+import { IconButton } from "@mui/material";
+import { Tooltip } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import ArticleIcon from "@mui/icons-material/Article";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Home</h1>
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <div className="flex flex-col items-center justify-center  w-full space-y-9">
+        <h1 className="text-5xl md:text-7xl font-semibold">Ahmed Elnour</h1>
+        <h2 className="text-xl md:text-2xl font-medium">
+          Full-Stack Developer
+        </h2>
+        <div className="flex items-end">
+          <LinkIcon
+            to="https://github.com/ahmedelnour5"
+            tipTitle="Github"
+            Icon={GitHubIcon}
+          >
+            <GitHubIcon sx={{ color: "#ffffff", fontSize: 50 }} />
+          </LinkIcon>
+
+          <LinkIcon
+            to="https://www.linkedin.com/in/ahmedelnoura/"
+            tipTitle="LinkedIn"
+          >
+            <LinkedInIcon sx={{ color: "#ffffff", fontSize: 50 }} />
+          </LinkIcon>
+          <LinkIcon
+            to="https://www.linkedin.com/in/ahmedelnoura/"
+            tipTitle="Resume"
+          >
+            <ArticleIcon sx={{ color: "#ffffff", fontSize: 50 }} />
+          </LinkIcon>
+        </div>
+      </div>
     </main>
   );
 }
+
+const LinkIcon = ({ to, tipTitle, children }) => {
+  return (
+    <Link href={to}>
+      <Tooltip title={tipTitle} placement="bottom">
+        <IconButton>{children}</IconButton>
+      </Tooltip>
+    </Link>
+  );
+};
