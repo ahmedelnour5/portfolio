1c2ec8ecd3b1a6010fe41a8cc0858774a2a02a72
Ahmed Elnour <ahmedelnour@Ahmeds-Air.fios-router.home>
2024-05-02 14:25:14 -0400

    attempt skills page fix

--- app/skills/page.jsx
+++ app/skills/page.jsx
@@ -28,7 +28,7 @@ const Skills = async () => {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center p-8 md:py-12">
       <div className="grid grid-cols-1 gap-6 scroll-smooth">
-        {skills.length > 0 ? (
+        {skills && skills.length > 0 ? (
           skills.map((skill) => <SkillsCard key={skill._id} skill={skill} />)
         ) : (
           <p>Whoops</p>
